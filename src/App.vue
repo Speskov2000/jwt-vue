85f1aea4f286e2c50124eb937df9946a28a7c66e
Peskov Sergey <serpesgeikov@gmail.com>
2021-11-26 18:04:40 +0600

    Can get token in localStorage and check userinfo by them

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/sign-up">Sigh up</router-link> |
+    <router-link to="/log-in">Log in</router-link>
   </div>
   <router-view/>
 </template>
+
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  beforeCreate() {
+    this.$store.commit("initializeStore")
+
+    const access = this.$store.state.access
+
+    if (access) {
+      axios.defaults.headers.common['Authorization'] = "JWT " + access
+    } else {
+      axios.defaults.headers.common['Authorization'] = ''
+    }
+  }
+}
+
+</script>
 
 <style lang="scss">
 #app {
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import SignUp from '../views/SignUp.vue'
+import LogIn from '../views/LogIn.vue'
 
 const routes = [
   {
@@ -10,11 +12,18 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/log-in',
+    name: 'LogIn',
+    component: LogIn
+  },
 ]
 
 const router = createRouter({
